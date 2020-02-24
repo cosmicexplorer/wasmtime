ddec282138824640bc77a975c0925b8fd29f376e
Deploy from CI <>
2020-02-24 19:49:57 +0000

    Deploy 13523e63125b6f8caef51a4491e4bc66b5db1cc5 to gh-pages

---
+++ api/yanix/file/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["FileType",""]],"fn":[["fionread","`fionread()` function, equivalent to `ioctl(fd, FIONREAD, *bytes)`."],["fstat",""],["fstatat",""],["isatty",""],["linkat",""],["mkdirat",""],["openat",""],["readlinkat",""],["renameat",""],["symlinkat",""],["unlinkat",""]],"struct":[["AtFlag",""],["FdFlag",""],["Mode",""],["OFlag",""]]});
